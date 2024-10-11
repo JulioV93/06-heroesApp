bf2d095d119109d9822580233a23f5bf38938a8d
Julio Machado <jmachado@vtsystems.cl>
2024-10-11 14:49:13 -0300

    Sección 15 - CRUD Heroes Sección 16 - Protección de Rutas

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environment } from '../../../environments/environments';
 
@@ -25,5 +25,22 @@ export class HeroesService {
 
     getSuggestions( query: string ): Observable<Hero[]> {
         return this.httpClient.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+    }
+
+    addHero(hero: Hero): Observable<Hero> {
+        return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`, hero);
+    }
+
+    updateHero(hero: Hero): Observable<Hero> {
+        if (!hero.id) throw new Error('The hero must have an id');
+        return this.httpClient.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+    }
+
+    deleteHeroById(id: string): Observable<boolean> {
+        return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`)
+            .pipe(
+                map(resp => true),
+                catchError(err => of(false)),
+            );
     }
 }
